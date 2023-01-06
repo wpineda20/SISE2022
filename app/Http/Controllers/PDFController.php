519c241b3f64598f1ec56a59d91b54f3dc8f6783
Giovani Tzec <giovanni.tzec@gmail.com>
2023-01-06 15:33:21 -0600

    reporte despacho por eje completo y todos en proceso

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cuscatlan\AxisCusca;
+use App\Models\Cuscatlan\Programmatic_Objective;
+use App\Models\Cuscatlan\TrakingCuscaMonthYearAction;
+
 use Barryvdh\DomPDF\Facade as DomPDF;
 use DB;
 
@@ -22,40 +25,117 @@ class PDFController extends Controller
 
         $data = $request->all();
 
-        $axis = [];
         $axis_title = "";
 
+        // Report All Axis
         if ($request->axis_description == "TODOS") {
-            $axis = AxisCusca::all();
-            // dd($axis);
+
             $axis_title = "Avance por ejes";
-            $axis_per = 100;
-        } else {
-            // $axis = AxisCusca::where('axis_description', $request->axis_description)->get();
-            $axis = DB::table('axis_cusca as ac')
-                ->select(
-                    'ac.*',
-                    'po.*',
-                    'sc.*',
-                    'rc.*',
-                    'rc.id as results_id',
-                    'act.*'
-                )
-                ->join('programmatic_objectives as po', 'po.axis_cusca_id', '=', 'ac.id')
-                ->join('strategy_cusca as sc', 'sc.programmatic_objectives_id', '=', 'po.id')
-                ->join('results_cusca as rc', 'rc.strategy_cusca_id', '=', 'sc.id')
-                ->join('actions_cusca as act', 'act.results_cusca_id', '=', 'rc.id')
-                ->where('axis_description', $request->axis_description)
+
+            $axis = AxisCusca::select('*')
                 ->get();
 
-            // dd($axis->toArray());
+            foreach ($axis as $key => $value) {
+                $value->trackings = DB::table('programmatic_objectives as po')
+                    ->select(
+                        'po.*',
+                        'sc.*',
+                        'rc.*',
+                        'act.id as actions_id',
+                        'act.year_goal_actions'
+                    )
+                    ->join('strategy_cusca as sc', 'sc.programmatic_objectives_id', '=', 'po.id', 'left outer')
+                    ->join('results_cusca as rc', 'rc.strategy_cusca_id', '=', 'sc.id', 'left outer')
+                    ->join('actions_cusca as act', 'act.results_cusca_id', '=', 'rc.id', 'left outer')
+                    ->where('po.axis_cusca_id', $value->id)
+                    ->get();
 
-            $axis_per = 100;
-            $axis_title = $request->axis_description;
+                $value->sum_year_goals = 0;
+                $value->sum_number_actions = 0;
+                $value->total_advanced_per = 0;
+
+                $value->sum_year_goals = $value->trackings->sum('year_goal_actions');
+
+                foreach ($value->trackings as $key => $item) {
+
+                    $item->details = TrakingCuscaMonthYearAction::select(
+                        'number_actions',
+                        'tracking_detail',
+                        'year_id',
+                        'y.year_name',
+                    )
+
+                        ->join('years as y', 'y.id', '=', 'year_id')
+                        ->where('actions_cusca_id', $item->actions_id)
+                        ->where('y.year_name', date('Y'))
+                        ->get();
+
+                    foreach ($item->details as $detail) {
+                        $item->total_number_actions = 0;
+                        $item->total_number_actions = $item->details->sum('number_actions');
+
+                        $value->sum_number_actions = $value->trackings->sum('total_number_actions');
+                    }
+                }
+                //Calculate Total Avanced Percentage
+                $value->total_advanced_per = $value->sum_number_actions /  $value->sum_year_goals * 100;
+            }
+
+            // dd($axis);
+
+            $pdf = DomPDF::loadView("cuscatlan.PDF.report", compact("data", "axis", "axis_title"))
+                ->setPaper("a4", "landscape");
+
+            return $pdf->stream("report-" . now() . ".pdf");
         }
 
+        // Report by Axis
+        $axis_title = $request->axis_description;
 
-        $pdf = DomPDF::loadView("cuscatlan.PDF.report", compact("data", "axis", "axis_title", "axis_per"))
+        $axis = AxisCusca::select(
+            '*',
+            'po.*',
+            'sc.*',
+            'rc.*',
+            'rc.id as results_id',
+            'act.id as actions_id',
+            'act.action_description',
+            'act.year_goal_actions'
+        )
+            ->join('programmatic_objectives as po', 'po.axis_cusca_id', '=', 'axis_cusca.id', 'left outer')
+            ->join('strategy_cusca as sc', 'sc.programmatic_objectives_id', '=', 'po.id', 'left outer')
+            ->join('results_cusca as rc', 'rc.strategy_cusca_id', '=', 'sc.id', 'left outer')
+            ->join('actions_cusca as act', 'act.results_cusca_id', '=', 'rc.id', 'left outer')
+            ->where('axis_cusca.axis_description', $request->axis_description)
+            ->get();
+
+        foreach ($axis as $key => $value) {
+
+            $value->trackings = TrakingCuscaMonthYearAction::select(
+                'number_actions',
+                'month_id',
+                'year_id',
+                'tracking_detail',
+                'actions_cusca_id',
+                'y.year_name',
+            )
+                ->join('years as y', 'y.id', '=', 'year_id')
+                ->where('actions_cusca_id', $value->actions_id)
+                ->where('y.year_name', date('Y'))
+                ->where('month_id', intval(date('n')))
+                ->get();
+
+            $goal = $value->year_goal_actions;
+
+            foreach ($value->trackings as $key => $item) {
+
+                $advance = $item->number_actions;
+
+                $item->per_progress = $advance / $goal * 100;
+            }
+        }
+
+        $pdf = DomPDF::loadView("cuscatlan.PDF.report", compact("data", "axis", "axis_title"))
             ->setPaper("a4", "landscape");
 
         return $pdf->stream("report-" . now() . ".pdf");
